7131843686f56ce0e5c06d8da82feb6c08832817
Oktasuke <t.okada.g@gmail.com>
2020-07-24 16:24:14 +0900

    Initial Commit

---
+++ Gandhi/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Gandhi
+//
+//  Created by taisuke.okada on 2020/07/24.
+//  Copyright © 2020 taisuke.okada. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
